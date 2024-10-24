cddf474a0bbfb6d74ba2bfb14a1b64a8c5bcb512
fortidemoscloud <fortidemoscloud@proton.me>
2024-10-24 14:55:01 +0200

    update Dockerfile and entryponit script

--- code/docker-entrypoint.sh
+++ code/docker-entrypoint.sh
@@ -8,7 +8,7 @@ sleep 5
 
 # Initialize the MySQL database if it doesn't already exist
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -u root -p "$DB_PASSWORD" < /var/www/html/bluebirdhotel.sql
+mysql -u root -p$DB_PASSWORD < /var/www/html/bluebirdhotel.sql
 
 # Start Apache service in the foreground
 apache2ctl -D FOREGROUND
